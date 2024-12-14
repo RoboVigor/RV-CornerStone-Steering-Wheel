c214b67aa319aff716e498f7992ccc27c1669f34
myqtmyq <YMao@2023.ljmu.ac.uk>
2024-12-14 21:26:11 +0800

    encoder update

--- driver/src/Driver_Encoder.c
+++ driver/src/Driver_Encoder.c
@@ -2,22 +2,22 @@
 #include "macro.h"
 #include "Driver_Encoder.h"
 
-void Encoder_Can_Send(CAN_TypeDef *CANx,uint8_t id, uint8_t command,uint32_t *data,uint8_t length){
-	uint8_t data_t[8];
-	uint8_t dlc;
-	uint8_t *data_l=(uint8_t *)data;
-	int i;
+void Encoder_Can_Send(CAN_TypeDef *CANx, uint8_t id, uint8_t command, uint32_t *data, uint8_t length) {
+    uint8_t  data_t[8];
+    uint8_t  dlc;
+    uint8_t *data_l = (uint8_t *) data;
+    int      i;
 	
-	dlc=length+3;
+    dlc = length + 3;
 	
-	data_t[0]=dlc;
-	data[1]=id;
-	data[2]=command;
-	for(i=3;i<dlc;i++){
-		data[i]=data_l[3+i-6];
+    data_t[0] = dlc;
+    data[1]   = id;
+    data[2]   = command;
+    for (i = 3; i < dlc; i++) {
+        data[i] = data_l[3 + i - 6];
 	}
 	
-    Can_Send_Msg(CANx,id,data_t,dlc);
+    Can_Send_Msg(CANx, id, data_t, dlc);
 	
 	return;
 	
