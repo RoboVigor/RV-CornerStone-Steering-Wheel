eeeddb5a70a2dda9e034c5cac7c299d3ca254169
myqtmyq <wangliya5020@163.com>
2025-02-24 03:36:09 +0800

    试着用成员函数写，剩下的明天搞

--- library/unitree_a1/unitree.h
+++ library/unitree_a1/unitree.h
@@ -4,24 +4,13 @@
 #include "stm32f4xx.h"
 #include "Driver_CAN.h"
 
-typedef enum{
-    Unitree_R,
-    Unitree_W
-}status_type;
+typedef enum { Unitree_R, Unitree_W } status_type;
 
-typedef enum{
-    Unitree_Mode1=10,
-    Unitree_Send_k,
-    Unitree_Read_k,
-    Unitree_Mode2
-}mode_type;
+typedef enum { Unitree_Mode1 = 10, Unitree_Send_k, Unitree_Read_k, Unitree_Mode2 } mode_type;
 
-typedef struct{
-    DFLT,
-    FOC,
-    CAL
-}motor_status_type;
-
+typedef struct {
+    DFLT, FOC, CAL
+} motor_status_type;
 
 typedef union {
     struct {
@@ -30,28 +19,39 @@ typedef union {
         uint32_t mode : 8;
         uint32_t motor_id : 4;
         uint32_t motor_status : 3;
-        uint32_t nc : 12; 
+        uint32_t nc : 12;
     };
-    uint32_t data; 
+    uint32_t data;
 } unitree_id_type;
 
 typedef union {
     struct {
         uint16_t angle;
-        uint8_t velocity;
+        uint8_t  velocity;
         uint8_t torque
     };
-    uint8_t data[4]; 
+    uint8_t data[4];
 } unitree_data_type;
 
-typedef struct{
-    unitree_id_type id_t;
+typedef struct {
+    unitree_id_type   id_t;
     unitree_data_type data_t;
-    uint8_t k_spd;
-    uint8_t k_pos;
+    uint8_t           k_spd;
+    uint8_t           k_pos;
 
-}Unitree_Type;
+    void (*_Unitree_Init)(struct Unitree_Type *unitree, uint8_t id, uint8_t motor_id, uint8_t k_spd, uint8_t k_pos);
 
-void Unitree_Init(Unitree_Type *unitree ,uint8_t id , uint8_t motor_id,uint8_t k_spd,uint8_t k_pos);
+    void (*_Unitree_Set_Id)(struct Unitree_Type *unitree, uint8_t id, uint8_t motor_id);
+    void (*_Unitree_Send)(struct Unitree_Type *unitree, uint8 *data);
+    void (*_Unitree_Set_K)(struct Unitree_Type *unitree, uint8_t k_spd, uint8_t k_pos);
+
+} Unitree_Type;
+
+void _Unitree_Init(struct Unitree_Type *unitree, uint8_t id, uint8_t motor_id, uint8_t k_spd, uint8_t k_pos);
+
+void _Unitree_Set_Id(struct Unitree_Type *unitree, uint8_t id, uint8_t motor_id);
+
+void _Unitree_Send(struct Unitree_Type *unitree, uint8 *data);
+void _Unitree_Set_K(struct Unitree_Type *unitree, uint8_t k_spd, uint8_t k_pos);
 
 #endif
